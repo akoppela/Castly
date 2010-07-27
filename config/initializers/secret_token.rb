c89b02a11729f2a6f022961c7873f6515c0d6204
dieinzige <dieinzige@me.com>
2010-07-27 20:31:07 +0400

    init apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Castly::Application.config.secret_token = '7fd5c92ffe6fd8c2093dceed9a128058a21a9671269e0f2b1c7c8d30548e27e70aa4ef02265820e625eb79078f4d3b8749ab8496ee56b3d88cc91889a8640a2e'
