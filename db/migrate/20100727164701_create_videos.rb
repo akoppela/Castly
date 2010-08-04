20d0c6992c2ec977e750916cae90a10b2d100491
dieinzige <dieinzige@me.com>
2010-08-04 13:06:29 +0400

    remove season identificator

--- db/migrate/20100727164701_create_videos.rb
+++ db/migrate/20100727164701_create_videos.rb
@@ -3,15 +3,25 @@ class CreateVideos < ActiveRecord::Migration
     create_table :videos do |t|
       t.references  :user,       :null => false
       t.references  :video_file
+      t.integer     :size #in megabytes
+      
+      t.integer     :play_count
+      t.timestamp   :last_played
+      
 
       t.string      :title,     :null => false
       t.integer     :sec_length,    :default => 0, :null => false #in sec
+      t.text        :description
+      
       t.boolean     :serial,    :default => false
       
       t.integer     :parent_id
       
       t.integer     :episode_number, :default => 0
-      t.references  :season
+      t.integer     :season_number,  :default => 0
+      
+      
+      
       
       t.integer     :sec_viewed, :default => 0, :null => false
       
