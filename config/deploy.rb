939d7589173137a2d4a74ca0f8af9d3b5d50d979
dieinzige <dieinzige@me.com>
2010-08-09 02:29:45 +0400

    update capify

---
+++ config/deploy.rb
@@ -0,0 +1,53 @@
+set :application, "castly"
+set :repository,  "git@blinr.unfuddle.com:blinr/castly.git"
+
+set :scm, :git
+set :user, "apps"
+set :use_sudo, false
+role :app, "castly.com"
+role :app, "castly.com"
+role :db,  "castly.com", :primary => true
+
+set :deploy_to, "/srv/apps/#{application}"
+
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    # run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "link the database.yml file over"
+  task :link_database_config do
+    run "ln -sF #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+
+  desc "use shared sockets dir"
+  task :link_sockets_dir do
+    run "mkdir -p #{shared_path}/sockets"
+    run "ln -sF #{shared_path}/sockets #{release_path}/tmp/sockets"
+  end
+  
+  task :bundle do
+    run "cd #{release_path} && RAILS_ENV=production bundle install #{shared_path}/bundle"
+  end
+end
+
+namespace :unicorn do
+  task :start do
+    run "(cd #{current_path} && unicorn -c config/unicorn.rb -E production -D)"
+  end
+  task :stop do
+    run "(cd #{current_path} && kill -QUIT `cat #{current_path}/tmp/pids/unicorn.pid`)"
+  end
+  task :restart do
+    run "(cd #{current_path} && kill -USR2 `cat #{current_path}/tmp/pids/unicorn.pid`)"
+  end
+end
+
+after 'deploy:update_code', 'deploy:bundle',
+                            'deploy:link_database_config',
+                            'deploy:link_sockets_dir'
+                            
+after "deploy:restart", "unicorn:restart"
---
+++ config/unicorn.rb
@@ -0,0 +1,62 @@
+RAILS_ROOT = File.expand_path(File.dirname(__FILE__) + "/../")
+rails_env = ENV['RACK_ENV'] || 'production'
+
+# 16 workers and 1 master
+worker_processes(rails_env == 'production' ? 4 : 1)
+
+# Load rails+github.git into the master before forking workers
+# for super-fast worker spawn times
+preload_app true
+
+# Restart any workers that haven't responded in 30 seconds
+timeout 30
+
+# Listen on a Unix data socket
+listen "#{RAILS_ROOT}/tmp/sockets/unicorn.sock", :backlog => 2048
+
+##
+# REE
+
+# http://www.rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
+
+
+before_fork do |server, worker|
+  
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
+  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
