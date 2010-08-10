24b15e06f35376aa389c9c313ebdb98f0a06a6c8
dieinzige <dieinzige@me.com>
2010-08-10 19:36:28 +0400

    prerelease

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   layout 'minimal'
   def index
-    redirect_to videos_path if current_user
-    # render "prerelease", :layout => "minimal"
+   # redirect_to videos_path if current_user
+   render "prerelease", :layout => "minimal"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Castly::Application.routes.draw do
-  resources :download_files
+  # resources :download_files
 
-  resources :invite_inquiries, :payments, :invites, :pages, :downloads, :videos, :users
+  resources :invite_inquiries#, :payments, :invites, :pages, :downloads, :videos, :users
   
-  resource :sessions
+  # resource :sessions
   root :to => "welcome#index"
-  
-  get 'login' => "sessions#new"
-  get 'signup' => "users#new"
-  get 'logout' => "sessions#destroy"
+  # 
+  # get 'login' => "sessions#new"
+  # get 'signup' => "users#new"
+  # get 'logout' => "sessions#destroy"
 end
